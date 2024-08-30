a9d4f25e57d6ea32520f33c9991cb803d46cbcb0
KevinHung <kevinaugust145@gmail.com>
2024-08-31 07:55:13 +0800

    Initial Commit

---
+++ MYEEOrderSystemTests/MYEEOrderSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  MYEEOrderSystemTests.swift
+//  MYEEOrderSystemTests
+//
+//  Created by 洪宗燦 on 2024/8/31.
+//
+
+import Testing
+@testable import MYEEOrderSystem
+
+struct MYEEOrderSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
